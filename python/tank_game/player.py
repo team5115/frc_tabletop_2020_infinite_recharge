a5abe2a0ee30c5e7826b080ce0c19254f811d205
jadams <jadams@jasmine>
2019-01-10 11:07:04 -0500

    No Comment

--- python/tank_game/player.py
+++ python/tank_game/player.py
@@ -4,11 +4,14 @@ from colors import *
 
 from pygame.math import Vector2
 
+
+# the rect element is used to blit the sprite
+
 class Player(pygame.sprite.Sprite):
     """ The class is the player-controlled sprite. """
 
     # -- Methods
-    def __init__(self, x, y,color=BLUE):
+    def __init__(self, x, y,color=BLUE, angle=45):
         """Constructor function"""
         # Call the parent's constructor
         super(Player,self).__init__()
@@ -31,38 +34,41 @@ class Player(pygame.sprite.Sprite):
         self.vx = 0
         self.vy = 0
 
-        #self.pointing_x=0
-        #self.pointing_y=0
-        #self.delta_angle=0
-        #self.angle=0
-
         self.position= Vector2(x,y)
         self.heading= Vector2(0,0)
         self.velocity= Vector2(0,0)
 #        self.accel= Vector2(0,0)
 
+#        self.heading.from_polar([1,angle])
+
+        self.set_heading_angle(angle)
+        
+    def set_heading_angle(self,theta):
+        self.heading.from_polar([1,theta])
+        self.rotate_rect(self.get_heading_angle())
+               
+    def get_heading_angle(self):
+        return self.heading.as_polar()[1]
+        
     def changespeed(self, a_x, a_y):
          """ Change the speed of the player"""
          self.velocity.x += a_x
          self.velocity.y += a_y
 
-    # def change_speed(self, acceleration):
-    #     self.vel=self.vel+acceleration
-
-
     def update(self):
         dt=1;
         self.position = self.position+dt*self.velocity
-        # """ Find a new position for the player"""
-        # self.rect.x += self.vx
-        # self.rect.y += self.vy
+        self.update_rect()  
+        
+    def update_rect(self):
         self.rect.x = self.position.x
         self.rect.y = self.position.y
         
         #if (self.delta_angle !=0):
         #    self.angle += self.delta_angle
         #    self.angle = self.angle % 360
-        #    self.rotate_rect(self.angle)
+        # self.rotate_rect(self.get_heading_angle())
+
             
             
     def rotate_rect(self,angle):
