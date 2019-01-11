f1c7617526b9b9c360b757249790e3b834aac5ac
jadams <jadams@jasmine>
2019-01-11 13:14:48 -0500

    converted top script to a class

--- python/tank_game/main.py
+++ python/tank_game/main.py
@@ -26,205 +26,219 @@ from colors import *
 from units import *
 
 
+class Game:
+
+    def __init__(self):
+        # Call this function so the Pygame library can initialize itself
+        pygame.init()
+
+        # Create an 800x600 sized screen
+        screen_size=[800,500]
+        self.screen = pygame.display.set_mode(screen_size,pygame.RESIZABLE)
+    
+        # Set the title of the window
+        pygame.display.set_caption('Test')
+
+
+        ##############################################
+        field_width=230*3
+        field_height=133*3
+
+        max_x=field_width;
+        max_y=field_height;
+
+        min_x=0
+        min_y=0
+        mid_x=max_x/2.0
+        mid_y=max_y/2.0
+
+        cargo_ship_xo=mid_x
+        cargo_ship_yo=mid_y
+
+        cargo_ship_1=Cargo_ship(cargo_ship_xo,cargo_ship_yo,GREEN)
+
+        rocket_1_xo=229
+        rocket_1_yo=0
+
+        rocket_2_xo=rocket_1_xo
+        rocket_2_yo=field_height-27
+
+        rocket_3_xo=field_width-rocket_1_xo
+        rocket_3_yo=rocket_1_yo
+
+        rocket_4_xo=rocket_3_xo
+        rocket_4_yo=rocket_2_yo
+
+        rocket_1=Rocket(rocket_1_xo,rocket_1_yo,BLUE)
+        rocket_2=Rocket(rocket_2_xo,rocket_2_yo,BLUE,180)
+        rocket_3=Rocket(rocket_3_xo,rocket_3_yo,RED)
+        rocket_4=Rocket(rocket_4_xo,rocket_4_yo,RED,180)
+
+        blue_hab_platform_level_3=Hab_platform_level_3(min_x,mid_y,HAB3,0)
+        y=blue_hab_platform_level_3.rect.bottom
+        blue_hab_platform_level_2b=Hab_platform_level_2(min_x,y,HAB2,0)
+        y=blue_hab_platform_level_3.rect.top
+        blue_hab_platform_level_2a=Hab_platform_level_2(min_x,y,HAB2,180)
+        x=blue_hab_platform_level_3.rect.right
+        blue_hab_platform_level_1=Hab_platform_level_1(x,mid_y,HAB1,0)
+
+        red_hab_platform_level_1=Hab_platform_level_1(max_x-3*ft_,mid_y,YELLOW,0)
+        red_hab_platform_level_2a=Hab_platform_level_2(max_x,mid_y,DARKGREEN,0)
+        red_hab_platform_level_2b=Hab_platform_level_2(max_x,mid_y,DARKGREEN,0)
+        red_hab_platform_level_3=Hab_platform_level_3(max_x,mid_y,RED,0)
+
+        #hab_zone_2=Hab_zone(max_x,mid_y,BLUE,180)
+
+        hab_zone_width=95
+        red_x=hab_zone_width/2
+        blue_x=field_width-(hab_zone_width/2)
+
+        p1_y=field_height/4
+        p2_y=p1_y*2
+        p3_y=p1_y*3
+
+
+        # Create the player object
+        self.player1 = Player(red_x, p1_y,RED1)
+        self.player2 = Player(red_x, p2_y,RED2)
+        self.player3 = Player(red_x, p3_y,RED3)
+
+        self.player4 = Player(blue_x, p1_y, BLUE1)
+        self.player5 = Player(blue_x, p2_y,BLUE2)
+        self.player6 = Player(blue_x, p3_y,BLUE3)
+
+        self.all_sprites_list = pygame.sprite.Group()
+        self.all_sprites_list.add(cargo_ship_1)
+        self.all_sprites_list.add(rocket_1)
+        self.all_sprites_list.add(rocket_2)
+        self.all_sprites_list.add(rocket_3)
+        self.all_sprites_list.add(rocket_4)
+        self.all_sprites_list.add(blue_hab_platform_level_1)
+        self.all_sprites_list.add(blue_hab_platform_level_2a)
+        self.all_sprites_list.add(blue_hab_platform_level_2b)
+        self.all_sprites_list.add(blue_hab_platform_level_3)
+        self.all_sprites_list.add(red_hab_platform_level_1)
+        self.all_sprites_list.add(red_hab_platform_level_2a)
+        self.all_sprites_list.add(red_hab_platform_level_2b)
+        self.all_sprites_list.add(red_hab_platform_level_3)
+
+        self.all_sprites_list.add(self.player1)
+        self.all_sprites_list.add(self.player2)
+        self.all_sprites_list.add(self.player3)
+        self.all_sprites_list.add(self.player4)
+        self.all_sprites_list.add(self.player5)
+        self.all_sprites_list.add(self.player6)
+
+        self.clock = pygame.time.Clock()
         
-# Call this function so the Pygame library can initialize itself
-pygame.init()
 
-# Create an 800x600 sized screen
-screen_size=[800,500]
-screen = pygame.display.set_mode(screen_size,pygame.RESIZABLE)
+    def redraw_screen(self):
+        field_width=230*3
+        field_height=133*3
 
-# Set the title of the window
-pygame.display.set_caption('Test')
+        max_x=field_width;
+        max_y=field_height;
 
-def redraw_screen():
-    field_width=230*3
-    field_height=133*3
+        min_x=0
+        min_y=0
+        mid_x=max_x/2.0
+        mid_y=max_y/2.0
 
-    max_x=field_width;
-    max_y=field_height;
 
-    min_x=0
-    min_y=0
-    mid_x=max_x/2.0
-    mid_y=max_y/2.0
+        # draw on the surface object
+        self.screen.fill(WHITE)
+        pygame.draw.polygon(self.screen, GREY, ((0,0), (field_width, 0), (field_width,field_height), (0,field_height), (0, 0)))
+    #    pygame.draw.line(self.screen, GREEN, (0,0), (field_width, 0), (field_width,field_height), (0,field_height), (0,0), 10)
+
+        pygame.draw.line(self.screen, YELLOW, (mid_x, min_y), (mid_x, max_y), 2)
+        pygame.draw.line(self.screen, YELLOW, (min_x, mid_y), (max_x, mid_y), 2)
+    #    pygame.draw.line(self.screen, BLUE, (120, 60), (60, 120))
+    #    pygame.draw.line(self.screen, BLUE, (60, 120), (120, 120), 4)
+    #    pygame.draw.circle(self.screen, BLUE, (300, 50), 20, 0)
+    #    pygame.draw.ellipse(self.screen, RED, (300, 250, 40, 80), 1)
+    #    pygame.draw.rect(self.screen, RED, (200, 150, 100, 50))
 
     
-    # draw on the surface object
-    screen.fill(WHITE)
-    pygame.draw.polygon(screen, GREY, ((0,0), (field_width, 0), (field_width,field_height), (0,field_height), (0, 0)))
-#    pygame.draw.line(screen, GREEN, (0,0), (field_width, 0), (field_width,field_height), (0,field_height), (0,0), 10)
+    def run(self):
+        d_angle=3
+        d_speed=3
+        done=False
+        
+        while not done:
 
-    pygame.draw.line(screen, YELLOW, (mid_x, min_y), (mid_x, max_y), 2)
-    pygame.draw.line(screen, YELLOW, (min_x, mid_y), (max_x, mid_y), 2)
-#    pygame.draw.line(screen, BLUE, (120, 60), (60, 120))
-#    pygame.draw.line(screen, BLUE, (60, 120), (120, 120), 4)
-#    pygame.draw.circle(screen, BLUE, (300, 50), 20, 0)
-#    pygame.draw.ellipse(screen, RED, (300, 250, 40, 80), 1)
-#    pygame.draw.rect(screen, RED, (200, 150, 100, 50))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    done = True
 
-##############################################
-field_width=230*3
-field_height=133*3
+                elif event.type == pygame.VIDEORESIZE:
+                    old_surface_saved = surface
+                    surface = pygame.display.set_mode((event.w, event.h),
+                                                      pygame.RESIZABLE)
+                    # On the next line, if only part of the window
+                    # needs to be copied, there's some other options.
+                    surface.blit(old_surface_saved, (0,0))
+                    del old_surface_saved     
 
-max_x=field_width;
-max_y=field_height;
+                # Set the speed based on the key pressed
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.player1.changespeed(-d_speed, 0)
+                    elif event.key == pygame.K_d:
+                        self.player1.changespeed(d_speed, 0)
+                    elif event.key == pygame.K_w:
+                        self.player1.changespeed(0, -d_speed)
+                    elif event.key == pygame.K_s:
+                        self.player1.changespeed(0, d_speed)
+                    elif event.key == pygame.K_q:
+                        self.player1.rotate(d_angle)
+                    elif event.key == pygame.K_e:
+                        self.player1.rotate(-d_angle)
 
-min_x=0
-min_y=0
-mid_x=max_x/2.0
-mid_y=max_y/2.0
-
-cargo_ship_xo=mid_x
-cargo_ship_yo=mid_y
-  
-cargo_ship_1=Cargo_ship(cargo_ship_xo,cargo_ship_yo,GREEN)
-
-rocket_1_xo=229
-rocket_1_yo=0
-
-rocket_2_xo=rocket_1_xo
-rocket_2_yo=field_height-27
-
-rocket_3_xo=field_width-rocket_1_xo
-rocket_3_yo=rocket_1_yo
-
-rocket_4_xo=rocket_3_xo
-rocket_4_yo=rocket_2_yo
-
-rocket_1=Rocket(rocket_1_xo,rocket_1_yo,BLUE)
-rocket_2=Rocket(rocket_2_xo,rocket_2_yo,BLUE,180)
-rocket_3=Rocket(rocket_3_xo,rocket_3_yo,RED)
-rocket_4=Rocket(rocket_4_xo,rocket_4_yo,RED,180)
-
-blue_hab_platform_level_3=Hab_platform_level_3(min_x,mid_y,HAB3,0)
-y=blue_hab_platform_level_3.rect.bottom
-blue_hab_platform_level_2b=Hab_platform_level_2(min_x,y,HAB2,0)
-y=blue_hab_platform_level_3.rect.top
-blue_hab_platform_level_2a=Hab_platform_level_2(min_x,y,HAB2,180)
-x=blue_hab_platform_level_3.rect.right
-blue_hab_platform_level_1=Hab_platform_level_1(x,mid_y,HAB1,0)
-
-red_hab_platform_level_1=Hab_platform_level_1(max_x-3*ft_,mid_y,YELLOW,0)
-red_hab_platform_level_2a=Hab_platform_level_2(max_x,mid_y,DARKGREEN,0)
-red_hab_platform_level_2b=Hab_platform_level_2(max_x,mid_y,DARKGREEN,0)
-red_hab_platform_level_3=Hab_platform_level_3(max_x,mid_y,RED,0)
-
-#hab_zone_2=Hab_zone(max_x,mid_y,BLUE,180)
-
-hab_zone_width=95
-red_x=hab_zone_width/2
-blue_x=field_width-(hab_zone_width/2)
-
-p1_y=field_height/4
-p2_y=p1_y*2
-p3_y=p1_y*3
+                # Reset speed when key goes up
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.player1.changespeed(d_speed, 0)
+                    elif event.key == pygame.K_d:
+                        self.player1.changespeed(-d_speed, 0)
+                    elif event.key == pygame.K_w:
+                        self.player1.changespeed(0, d_speed)
+                    elif event.key == pygame.K_s:
+                        self.player1.changespeed(0, -d_speed)
+                    elif event.key == pygame.K_q:
+                        self.player1.rotate(-d_angle)
+                    elif event.key == pygame.K_e:
+                        self.player1.rotate(d_angle)
 
 
-# Create the player object
-player1 = Player(red_x, p1_y,RED1)
-player2 = Player(red_x, p2_y,RED2)
-player3 = Player(red_x, p3_y,RED3)
+            # This actually moves the player block based on the current speed
+            self.player1.update(self.all_sprites_list)
+            #self.player2.update()
+            #self.player3.update()
+            #self.player4.update()
+            #self.player5.update()
+            #self.player6.update()
 
-player4 = Player(blue_x, p1_y, BLUE1)
-player5 = Player(blue_x, p2_y,BLUE2)
-player6 = Player(blue_x, p3_y,BLUE3)
+            # -- Draw everything
+            # Clear self.screen
+            #self.screen.fill(WHITE)
+            self.redraw_screen()
 
-all_sprites_list = pygame.sprite.Group()
-all_sprites_list.add(cargo_ship_1)
-all_sprites_list.add(rocket_1)
-all_sprites_list.add(rocket_2)
-all_sprites_list.add(rocket_3)
-all_sprites_list.add(rocket_4)
-all_sprites_list.add(blue_hab_platform_level_1)
-all_sprites_list.add(blue_hab_platform_level_2a)
-all_sprites_list.add(blue_hab_platform_level_2b)
-all_sprites_list.add(blue_hab_platform_level_3)
-all_sprites_list.add(red_hab_platform_level_1)
-all_sprites_list.add(red_hab_platform_level_2a)
-all_sprites_list.add(red_hab_platform_level_2b)
-all_sprites_list.add(red_hab_platform_level_3)
+            # Draw sprites
+            self.all_sprites_list.draw(self.screen)
 
-all_sprites_list.add(player1)
-all_sprites_list.add(player2)
-all_sprites_list.add(player3)
-all_sprites_list.add(player4)
-all_sprites_list.add(player5)
-all_sprites_list.add(player6)
+            # Flip screen
+            pygame.display.flip()
 
-clock = pygame.time.Clock()
-done = False
+            # Pause
+            self.clock.tick(60)
 
-d_angle=3
-d_speed=3
+            #if self.player1.is_collided_with(self.player2):
+            #    print "COLLISION"
 
-while not done:
+        pygame.quit()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+if __name__ == '__main__':
 
-            
-        elif event.type == VIDEORESIZE:
-            # There's some code to add back window content here.
-            surface = pygame.display.set_mode((event.w, event.h),
-                                              pygame.RESIZABLE)
-#            CreateWindow(event.w,event.h)
-            
-        # Set the speed based on the key pressed
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_a:
-                player1.changespeed(-d_speed, 0)
-            elif event.key == pygame.K_d:
-                player1.changespeed(d_speed, 0)
-            elif event.key == pygame.K_w:
-                player1.changespeed(0, -d_speed)
-            elif event.key == pygame.K_s:
-                player1.changespeed(0, d_speed)
-            elif event.key == pygame.K_q:
-                player1.rotate(d_angle)
-            elif event.key == pygame.K_e:
-                player1.rotate(-d_angle)
-
-        # Reset speed when key goes up
-        elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_a:
-                player1.changespeed(d_speed, 0)
-            elif event.key == pygame.K_d:
-                player1.changespeed(-d_speed, 0)
-            elif event.key == pygame.K_w:
-                player1.changespeed(0, d_speed)
-            elif event.key == pygame.K_s:
-                player1.changespeed(0, -d_speed)
-            elif event.key == pygame.K_q:
-                player1.rotate(-d_angle)
-            elif event.key == pygame.K_e:
-                player1.rotate(d_angle)
-
-
-    # This actually moves the player block based on the current speed
-    player1.update(all_sprites_list)
-    #player2.update()
-    #player3.update()
-    #player4.update()
-    #player5.update()
-    #player6.update()
-
-    # -- Draw everything
-    # Clear screen
-    #screen.fill(WHITE)
-    redraw_screen()
+    the_game=Game()
+    the_game.run();
     
-    # Draw sprites
-    all_sprites_list.draw(screen)
-
-    # Flip screen
-    pygame.display.flip()
-
-    # Pause
-    clock.tick(60)
-
-    #if player1.is_collided_with(player2):
-    #    print "COLLISION"
-    
-pygame.quit()
